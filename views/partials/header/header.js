581ecf5308ffe330640160ffcb8eed0383dfc519
mikhalev <andevme@gmail.com>
2020-02-29 19:52:06 +0300

    complete header

--- views/partials/header/header.js
+++ views/partials/header/header.js
@@ -1,26 +1,10 @@
 export const headerMenu = () => {
-    // const headerNode = document.querySelector(`header`);
-    // const sandwichButton = headerNode.querySelector(`.headerSandwichButton`);
-    // sandwichButton.addEventListener(`click`, () => {
-    //     const isMenuActive = headerNode.classList.contains(`activeMenu`);
-    //     const classAction = (isMenuActive) ? `remove` : `add`;
-    //     headerNode.classList[classAction](`activeMenu`);
-    // });
-    // const dropdownLinks = [...headerNode.querySelectorAll(`.dropdownMenuLink`)];
-    // dropdownLinks.forEach((link) => {
-    //     const linkWrapper =  link.parentNode;
-    //     link.addEventListener(`click`, () => {
-    //         const isActive = linkWrapper.classList.contains(`activeDropdownLink`);
-    //         const classAction = (isActive) ? `remove` : `add`;
-    //         const menuHeight = linkWrapper.querySelector(`.subMenuList`).offsetHeight;
-    //         link.parentNode.classList[classAction](`activeDropdownLink`);
-    //         link.parentNode.style.height = (isActive) ? `20px` : `${menuHeight + 35}px`;
-    //     });
-    // });
 
     const headerNode = document.querySelector(`header`);
+    if (!headerNode) return false;
 
     const sandwichButton = headerNode.querySelector(`.headerSandwichButton`);
+    if (!sandwichButton) return false;
     sandwichButton.addEventListener(`click`, () => {
         const isMenuActive = headerNode.classList.contains(`activeMenu`);
         const classAction = (isMenuActive) ? `remove` : `add`;
@@ -28,6 +12,7 @@ export const headerMenu = () => {
     });
 
     const dropdownLinks = [...headerNode.querySelectorAll(`.dropdownMenuLink`)];
+    if (!dropdownLinks.length) return false;
     dropdownLinks.forEach((link) => {
         const linkWrapper =  link.parentNode;
         link.addEventListener(`click`, () => {
@@ -36,4 +21,5 @@ export const headerMenu = () => {
             link.parentNode.classList[classAction](`activeDropdownLink`);
         });
     });
+
 };
