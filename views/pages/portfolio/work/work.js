695bcf8c7b05460075c05b4988c78fcd7ff76b46
mikhalev <andevme@gmail.com>
2020-10-26 17:48:54 +0300

    update portfolio previews

--- views/pages/portfolio/work/work.js
+++ views/pages/portfolio/work/work.js
@@ -1,7 +1,7 @@
 export const workImages = () => {
     const previewImages = [...document.querySelectorAll(`.previewImage`)];
+    const previewLinks = [...document.querySelectorAll(`.previewLink`)];
     const workImage = document.querySelector(`.fullsizePicture`);
-    const workLink = workImage.closest(`.fullsizePictureLink`);
     const loader = document.querySelector(`.fullsizeLoader`);
     if (workImage) {
         workImage.addEventListener(`load`, () => {
@@ -9,13 +9,26 @@ export const workImages = () => {
             workImage.classList.remove(`hiddenPicture`);
         });
     }
+    const checkPreviewClasses = (clickedNode) => {
+        previewLinks.forEach((previewLink) => {
+            const previewImage = previewLink.parentNode.querySelector(`.previewImage`);
+            previewLink.setAttribute(`href`, previewImage.dataset.picture);
+            previewLink.dataset.fslightbox = `gallery`;
+        });
+        const currentParent = clickedNode.closest(`.previewWrapper`);
+        const currentPreviewLink = currentParent.querySelector(`.previewLink`);
+        currentPreviewLink.removeAttribute(`href`);
+        currentPreviewLink.removeAttribute(`data-fslightbox`);
+    };
     const showImage = (event) => {
-        const { target: { dataset: { picture }}} = event;
+        const { target: { dataset: { picture }}, target: clickedNode } = event;
+        const workLink = workImage.closest(`.fullsizePictureLink`);
         workImage.classList.add(`hiddenPicture`);
         loader.classList.remove(`hiddenLoader`);
         setTimeout(() => {
             workImage.src = picture;
             workLink.setAttribute(`href`, picture);
+            checkPreviewClasses(clickedNode);
             refreshFsLightbox();
         }, 500);
     };
