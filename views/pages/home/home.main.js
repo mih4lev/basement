a4d22a0e6e427d1364bb576514ca4a3b9eab1422
mikhalev <andevme@gmail.com>
2020-10-31 23:44:06 +0300

    add slideshow header func

--- views/pages/home/home.main.js
+++ views/pages/home/home.main.js
@@ -1,8 +1,19 @@
 import { testimonialsCarousel, youtubeVideo } from "../../partials/blocks/testimonials/testimonials";
 import { middleCategories } from "../../partials/micro-blocks/middle-categories/middle-categories";
 import { offersIcons } from "../../partials/micro-blocks/offers-buttons/offers-buttons";
+import Swiper from 'swiper/bundle';
 
 testimonialsCarousel();
 youtubeVideo();
 middleCategories();
 offersIcons();
+
+new Swiper(`.swiper-container`, {
+    direction: `vertical`,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    }
+});
--- views/pages/landing/landing.main.js
+++ views/pages/landing/landing.main.js
@@ -1,4 +1,15 @@
 import { testimonialsCarousel, youtubeVideo } from "../../partials/blocks/testimonials/testimonials";
+import Swiper from 'swiper/bundle';
 
 testimonialsCarousel();
 youtubeVideo();
+
+new Swiper(`.swiper-container`, {
+    direction: `vertical`,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    }
+});
