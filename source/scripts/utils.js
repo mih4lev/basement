1dce51217598b04f957eedbadbc7733e3536413f
mikhalev <andevme@gmail.com>
2020-11-01 19:39:32 +0300

    update slideshow

--- source/scripts/utils.js
+++ source/scripts/utils.js
@@ -324,7 +324,6 @@ export const topSlider = () => {
     new Swiper(`.swiper-container`, {
         direction: `vertical`,
         spaceBetween: 30,
-        centeredSlides: true,
         autoplay: {
             delay: 2500,
             disableOnInteraction: false
